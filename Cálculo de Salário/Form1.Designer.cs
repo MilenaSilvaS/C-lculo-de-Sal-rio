7b3d80c296b8fa23a05c0ef4a568ef26c5743807
User <User@DESKTOP-AIDJU9G>
2023-03-21 00:44:28 -0300

    Adicionar arquivos de projeto.

---
+++ Cálculo de Salário/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace Cálculo_de_Salário
+{
+    partial class FrmCalculoDeSalario
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCalculoDeSalario));
+            this.lblSalarioMinino = new System.Windows.Forms.Label();
+            this.lblHorasTrabalhadas = new System.Windows.Forms.Label();
+            this.txtSalarioM = new System.Windows.Forms.TextBox();
+            this.txtHorasT = new System.Windows.Forms.TextBox();
+            this.gbxCategoria = new System.Windows.Forms.GroupBox();
+            this.rbnVeterano = new System.Windows.Forms.RadioButton();
+            this.rbnCalouro = new System.Windows.Forms.RadioButton();
+            this.gbxTurno = new System.Windows.Forms.GroupBox();
+            this.rbnNoturno = new System.Windows.Forms.RadioButton();
+            this.rbnVespertino = new System.Windows.Forms.RadioButton();
+            this.rbnMatutino = new System.Windows.Forms.RadioButton();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lbxResumo = new System.Windows.Forms.ListBox();
+            this.txtSituacaoEstagiario = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.gbxCategoria.SuspendLayout();
+            this.gbxTurno.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSalarioMinino
+            // 
+            this.lblSalarioMinino.AutoSize = true;
+            this.lblSalarioMinino.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalarioMinino.Location = new System.Drawing.Point(22, 23);
+            this.lblSalarioMinino.Name = "lblSalarioMinino";
+            this.lblSalarioMinino.Size = new System.Drawing.Size(100, 16);
+            this.lblSalarioMinino.TabIndex = 0;
+            this.lblSalarioMinino.Text = "Salário Mínimo:";
+            // 
+            // lblHorasTrabalhadas
+            // 
+            this.lblHorasTrabalhadas.AutoSize = true;
+            this.lblHorasTrabalhadas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHorasTrabalhadas.Location = new System.Drawing.Point(21, 57);
+            this.lblHorasTrabalhadas.Name = "lblHorasTrabalhadas";
+            this.lblHorasTrabalhadas.Size = new System.Drawing.Size(129, 16);
+            this.lblHorasTrabalhadas.TabIndex = 1;
+            this.lblHorasTrabalhadas.Text = "Horas Trabalhadas:";
+            // 
+            // txtSalarioM
+            // 
+            this.txtSalarioM.Location = new System.Drawing.Point(170, 21);
+            this.txtSalarioM.Name = "txtSalarioM";
+            this.txtSalarioM.Size = new System.Drawing.Size(144, 21);
+            this.txtSalarioM.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.txtSalarioM, "Informe o valor atual para o salário mínimo");
+            // 
+            // txtHorasT
+            // 
+            this.txtHorasT.Location = new System.Drawing.Point(171, 53);
+            this.txtHorasT.Name = "txtHorasT";
+            this.txtHorasT.Size = new System.Drawing.Size(144, 21);
+            this.txtHorasT.TabIndex = 3;
+            this.toolTip2.SetToolTip(this.txtHorasT, "Digite a quantidade de horas trabalhadas\r\n\r\n");
+            // 
+            // gbxCategoria
+            // 
+            this.gbxCategoria.Controls.Add(this.rbnVeterano);
+            this.gbxCategoria.Controls.Add(this.rbnCalouro);
+            this.gbxCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxCategoria.Location = new System.Drawing.Point(20, 82);
+            this.gbxCategoria.Name = "gbxCategoria";
+            this.gbxCategoria.Size = new System.Drawing.Size(296, 65);
+            this.gbxCategoria.TabIndex = 4;
+            this.gbxCategoria.TabStop = false;
+            this.gbxCategoria.Text = "Categoria";
+            // 
+            // rbnVeterano
+            // 
+            this.rbnVeterano.AutoSize = true;
+            this.rbnVeterano.Location = new System.Drawing.Point(154, 28);
+            this.rbnVeterano.Name = "rbnVeterano";
+            this.rbnVeterano.Size = new System.Drawing.Size(81, 20);
+            this.rbnVeterano.TabIndex = 1;
+            this.rbnVeterano.TabStop = true;
+            this.rbnVeterano.Text = "Veterano";
+            this.rbnVeterano.UseVisualStyleBackColor = true;
+            // 
+            // rbnCalouro
+            // 
+            this.rbnCalouro.AutoSize = true;
+            this.rbnCalouro.Location = new System.Drawing.Point(22, 28);
+            this.rbnCalouro.Name = "rbnCalouro";
+            this.rbnCalouro.Size = new System.Drawing.Size(73, 20);
+            this.rbnCalouro.TabIndex = 0;
+            this.rbnCalouro.TabStop = true;
+            this.rbnCalouro.Text = "Calouro";
+            this.rbnCalouro.UseVisualStyleBackColor = true;
+            // 
+            // gbxTurno
+            // 
+            this.gbxTurno.Controls.Add(this.rbnNoturno);
+            this.gbxTurno.Controls.Add(this.rbnVespertino);
+            this.gbxTurno.Controls.Add(this.rbnMatutino);
+            this.gbxTurno.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxTurno.Location = new System.Drawing.Point(372, 3);
+            this.gbxTurno.Name = "gbxTurno";
+            this.gbxTurno.Size = new System.Drawing.Size(126, 144);
+            this.gbxTurno.TabIndex = 5;
+            this.gbxTurno.TabStop = false;
+            this.gbxTurno.Text = "Turno";
+            // 
+            // rbnNoturno
+            // 
+            this.rbnNoturno.AutoSize = true;
+            this.rbnNoturno.Location = new System.Drawing.Point(17, 100);
+            this.rbnNoturno.Name = "rbnNoturno";
+            this.rbnNoturno.Size = new System.Drawing.Size(73, 20);
+            this.rbnNoturno.TabIndex = 2;
+            this.rbnNoturno.TabStop = true;
+            this.rbnNoturno.Text = "Noturno";
+            this.rbnNoturno.UseVisualStyleBackColor = true;
+            // 
+            // rbnVespertino
+            // 
+            this.rbnVespertino.AutoSize = true;
+            this.rbnVespertino.Location = new System.Drawing.Point(16, 65);
+            this.rbnVespertino.Name = "rbnVespertino";
+            this.rbnVespertino.Size = new System.Drawing.Size(91, 20);
+            this.rbnVespertino.TabIndex = 1;
+            this.rbnVespertino.TabStop = true;
+            this.rbnVespertino.Text = "Vespertino";
+            this.rbnVespertino.UseVisualStyleBackColor = true;
+            // 
+            // rbnMatutino
+            // 
+            this.rbnMatutino.AutoSize = true;
+            this.rbnMatutino.Location = new System.Drawing.Point(16, 29);
+            this.rbnMatutino.Name = "rbnMatutino";
+            this.rbnMatutino.Size = new System.Drawing.Size(76, 20);
+            this.rbnMatutino.TabIndex = 0;
+            this.rbnMatutino.TabStop = true;
+            this.rbnMatutino.Text = "Matutino";
+            this.rbnMatutino.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(328, 300);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(87, 30);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lbxResumo
+            // 
+            this.lbxResumo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxResumo.FormattingEnabled = true;
+            this.lbxResumo.ItemHeight = 16;
+            this.lbxResumo.Location = new System.Drawing.Point(19, 155);
+            this.lbxResumo.MultiColumn = true;
+            this.lbxResumo.Name = "lbxResumo";
+            this.lbxResumo.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lbxResumo.Size = new System.Drawing.Size(483, 116);
+            this.lbxResumo.TabIndex = 8;
+            // 
+            // txtSituacaoEstagiario
+            // 
+            this.txtSituacaoEstagiario.BackColor = System.Drawing.Color.Yellow;
+            this.txtSituacaoEstagiario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSituacaoEstagiario.ForeColor = System.Drawing.Color.Blue;
+            this.txtSituacaoEstagiario.Location = new System.Drawing.Point(20, 300);
+            this.txtSituacaoEstagiario.Name = "txtSituacaoEstagiario";
+            this.txtSituacaoEstagiario.Size = new System.Drawing.Size(300, 26);
+            this.txtSituacaoEstagiario.TabIndex = 9;
+            this.txtSituacaoEstagiario.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "Ajuda";
+            this.toolTip1.UseAnimation = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(405, 286);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(117, 58);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // toolTip2
+            // 
+            this.toolTip2.IsBalloon = true;
+            this.toolTip2.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip2.ToolTipTitle = "Ajuda";
+            // 
+            // FrmCalculoDeSalario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 350);
+            this.Controls.Add(this.txtSituacaoEstagiario);
+            this.Controls.Add(this.lbxResumo);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.gbxTurno);
+            this.Controls.Add(this.gbxCategoria);
+            this.Controls.Add(this.txtHorasT);
+            this.Controls.Add(this.txtSalarioM);
+            this.Controls.Add(this.lblHorasTrabalhadas);
+            this.Controls.Add(this.lblSalarioMinino);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmCalculoDeSalario";
+            this.Text = "Cálculo de Salário";
+            this.gbxCategoria.ResumeLayout(false);
+            this.gbxCategoria.PerformLayout();
+            this.gbxTurno.ResumeLayout(false);
+            this.gbxTurno.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSalarioMinino;
+        private System.Windows.Forms.Label lblHorasTrabalhadas;
+        private System.Windows.Forms.TextBox txtSalarioM;
+        private System.Windows.Forms.TextBox txtHorasT;
+        private System.Windows.Forms.GroupBox gbxCategoria;
+        private System.Windows.Forms.RadioButton rbnVeterano;
+        private System.Windows.Forms.RadioButton rbnCalouro;
+        private System.Windows.Forms.GroupBox gbxTurno;
+        private System.Windows.Forms.RadioButton rbnNoturno;
+        private System.Windows.Forms.RadioButton rbnVespertino;
+        private System.Windows.Forms.RadioButton rbnMatutino;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ListBox lbxResumo;
+        private System.Windows.Forms.TextBox txtSituacaoEstagiario;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolTip toolTip2;
+    }
+}
+
